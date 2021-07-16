18a049e5f38f19dea8cf2c3b875bf2be30ea1ca5
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2021-07-16 23:47:54 +0900

    lesson4 done

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout, {siteTitle} from '../components/layout'
 import styled from 'styled-components'
 import {getSortedPostsData} from '../lib/posts'
 
-export default function Home() {
+export default function Home({allPostsData}) {
     return (
         <Layout home>
             <Head>
@@ -15,6 +15,18 @@ export default function Home() {
                     (This is a sample website - you’ll be building a site like this on{' '}
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
                 </p>
+                <Header2>Blog</Header2>
+                <UList>
+                    {allPostsData.map(({id, date, title}) => (
+                        <ListItem key={id}>
+                            {title}
+                            <br/>
+                            {id}
+                            <br/>
+                            {date}
+                        </ListItem>
+                    ))}
+                </UList>
             </Introduction>
         </Layout>
     )
@@ -32,4 +44,22 @@ export async function getStaticProps() {
 const Introduction = styled.section`
   font-size: 1.2rem;
   line-height: 1.5;
+`;
+
+const Header2 = styled.h2`
+  font-size: 2rem;
+  line-height: 1.3;
+  font-weight: 800;
+  letter-spacing: -0.05rem;
+  margin: 1rem 0;
+`
+
+const UList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const ListItem = styled.li`
+  margin: 0 0 1.25rem;
 `;
